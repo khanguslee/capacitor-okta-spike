ff2719f413dbbe84a67d964d3bde08abce65be4c
khanguslee <khanguslee@gmail.com>
2019-06-03 17:57:49 +1000

    Create some sort of authentication with okta

--- react-capacitor-okta/src/App.js
+++ react-capacitor-okta/src/App.js
@@ -1,33 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Security, ImplicitCallback } from '@okta/okta-react';
-import logo from './logo.svg';
 import './App.css';
+
+// Pages
+import Home from './Home';
+
 const config = require('./okta.config.json');
 
 const oktaConfig = {
-  issuer: config.organisationUrl,
+  issuer: `${config.organisationUrl}/oauth2/default`,
   redirect_uri: `${window.location.origin}/implicit/callback`,
   client_id: config.clientId,
 };
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Security issuer={oktaConfig.issuer}
+                client_id={oktaConfig.client_id}
+                redirect_uri={oktaConfig.redirect_uri}
+      >
+        <Route path='/' exact={true} component={Home}/>
+        <Route path='/implicit/callback' component={ImplicitCallback}/>
+      </Security>
+    </Router>
   );
 }
 
